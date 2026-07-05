022226248cb98e5454a6fd20a7a38e229e046c03
Zaid Khan <zk76159@gmail.com>
2026-07-05 14:24:14 +0530

    Added Folder Structure

---
+++ src/main/java/com/zaid/lld/playground/Main.java
@@ -0,0 +1,13 @@
+package com.zaid.lld.playground;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Java LLD 🚀");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
